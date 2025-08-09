4f5702d82e1ba242d2dcf847603e054e8e26632e
jxx-mx <fierce.unarmed_0l@icloud.com>
2025-08-10 04:06:51 +0900

    fix: navigation

--- components/top-header.tsx
+++ components/top-header.tsx
@@ -7,12 +7,20 @@ import {
 } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { Navigation } from "@/components/navigation";
+import { currentUser } from "@clerk/nextjs/server";
 
 export async function TopHeader() {
+  const user = await currentUser();
   return (
     <header className="mb-4">
       <div className="flex justify-between items-center max-w-7xl mx-auto p-4">
-        <Navigation />
+        {user ? (
+          <Navigation />
+        ) : (
+          <div className="text-sm font-semibold flex items-center gap-2">
+            <p>ISSUE CHECK</p>
+          </div>
+        )}
         <div className="flex items-center gap-2">
           <SignedOut>
             <SignInButton>
